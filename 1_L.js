fb0747d12031a6ab8a6b01fb2ef44bc772beded8
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-04-20 23:53:42 +0530

    1_L.js

--- 1_L.js
+++ 1_L.js
@@ -1,7 +1,7 @@
 
 // Coding Challenge #4
 
-/*
+/* 
 Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
