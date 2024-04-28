1a5b9ec7a64a2bf2aec82e8ab5624306f7dadd6f
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-04-28 23:38:07 +0530

    1F.js

--- 1F.js
+++ 1F.js
@@ -8,7 +8,7 @@ if (age >= 18) {
   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
 
-const birthYear = 2012;
+const birthYear = 2012; 
 
 let century;
 if (birthYear <= 2000) {
